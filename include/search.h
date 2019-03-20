0355366c0e32c8667ae564a93a0e2dbee3e101cf
matheusmas132 <matheus.mas132@ufrn.edu.br>
2019-03-19 21:07:48 -0300

    doxygen format comments

--- include/search.h
+++ include/search.h
@@ -3,18 +3,47 @@
 
 #include <cmath> 	// sqrt()
 
+
+/*! \brief Linear Search Algorithm
+ *
+ *  Linear Search code
+ */
 int linearSearchIterative(long int arr[], int l, int r, long int value, long int &stepcounter); 
 
+/*! \brief Iterative Binary Search Algorithm
+ *
+ *  Iterative Binary Search code
+ */
 int binarySearchIterative(long int arr[], int l, int r, long int value, long int &stepcounter);
 
+/*! \brief Recursive Binary Search Algorithm
+ *
+ *  Recursive Binary Search code
+ */
 int binarySearchRecursive(long int arr[], int l, int r, long int value, long int &stepcounter);
 
+/*! \brief Iterative Ternary Search Algorithm
+ *
+ *  Iterative Ternary Search code
+ */
 int ternarySearchIterative(long int arr[], int l, int r, long int value, long int &stepcounter);
 
+/*! \brief Recursive Ternary Search Algorithm
+ *
+ *  ternary Search Recursive code
+ */
 int ternarySearchRecursive(long int arr[], int l, int r, long int value, long int &stepcounter);
 
+/*! \brief Jump Search Algorithm
+ *
+ *  jump Search code
+ */
 int jumpSearch(long int arr[], int l, int r, long int value, long int &stepcounter);
 
+/*! \brief Fibonacci Search Algorithm
+ *
+ *  Fibonacci Search code
+ */
 int fibonacciSearch(long int arr[], int l, int r, long int value, long int &stepcounter);
 
 #endif
--- include/util.h
+++ include/util.h
@@ -5,12 +5,29 @@
 #include <new>
 #include <string>
 
+/*! \brief Test Memory Limit
+ *
+ *  Try to alloc 100.000.000 to 500.000.000 long ints and return the max of the successful allocations
+ */
 int setMemoryLimit(void);
 
+/*! \brief Show a message error
+ *
+ *  send a number and show in terminal a message error of the namber
+ */
 void showErrorMessage(int number);
 
+/*! \brief Number of tests menu
+ *
+ *  Show a menu, get input of number of tests, clean input and return an int
+ */
 int numberOfTestsMenu(void);
 
+/*! \brief Set algorithm menu
+ *
+ *  Show a menu, get input of algorithms to be tested and return a string with each
+ *  char is a algorithm number.
+ */
 std::string setAlgorithmsMenu(void);
 
 #endif
